d0b51b22babafba371da133ed5a58d8651598cbe
bagayboss509 <107231147+bagayboss509@users.noreply.github.com>
2024-11-16 16:30:37 -0500

    shizuku.sh

---
+++ shizuku.sh
@@ -0,0 +1,4 @@
+curl "https://ia903204.us.archive.org/27/items/rish_20241116/rish?alt=media&token=ea22ed0c-6312-4c95-9121-d7e5392d3e31" -o rish
+curl "https://archive.org/download/rish_20241116/rish_shizuku.dex?alt=media&token=4e72a95a-57fc-4ce8-9cf0-5afb2342326c" -o rish_shizuku.dex
+chmod +rwx rish
+chmod +rwx rish_shizuku.dex
